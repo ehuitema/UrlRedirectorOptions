a6b1f0397fd23cc688890ff45d49fbe3d6583236
Edwin Huitema <ehuitema@Pegamento.nl>
2017-03-13 16:37:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+URLRedirectorOptions
+====================
+
+## Description
+
+This widget enables you to redirect to a URL. It is a fork from the UrlRedirector widget with additional options
+
+## Contributing
+For more information on contributing to this repository visit [Contributing to a GitHub repository] (https://world.mendix.com/display/howto50/Contributing+to+a+GitHub+repository)
+
+## Typical usage scenario
+
+Redirect to a static url or a URL stored as a value in an object.
+
+## Features and limitations
+ 
+- Open URL in a new or current browser window 
+- Can add additional options for the window.open command
+- Combine prefix URL with a dynamic URL part
+
+## Installation
+
+Import the widget to your project and add the widget to a dataview on a page. Configure the properties to determine how the widget will behave in your application.
+
+###Properties
+
+
+##### General
+
+###### Data source
+
+* *Options* - Options used in the window.open function. E.g. 'resizeable,toolbar=yes,location=yes'
+* *URL(prefix)* - URL to redirect to. (E.g. http://www.mendix.com)
+* *URL Attribute* - Attribute containing a URL value to redirect to. (E.g. http://www.mendix.com, you could also combine with the prefix value and add the value '/learn' to it)
+* *Command Attribute* - Attribute containing the command to handle the url. Possible command values are: 'open', 'close', 'redirect'. 'redirect' will open the url in the same page.
+* *Target* - Window where the redirect URL should be opened 
+
---
+++ src/URLRedirectorOptions/widget/URLRedirectorOptions.js
@@ -0,0 +1,176 @@
+/*jslint white:true, nomen: true, plusplus: true */
+/*global mx, mxui, define, require, browser, devel, console, document, jQuery, window */
+/*mendix */
+/*
+    HelpText
+    ========================
+
+    @file      : URLRedirectorOptions.js
+    @version   : 1.0
+    @author    : EH
+    @date      : Tue, 03 Jun 2015 11:19:00 GMT
+    @copyright : Mendix bv
+    @license   : Apache 2
+
+    Documentation
+    ========================
+    Describe your widget here.
+*/
+
+// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
+define([
+    'dojo/_base/declare', 'mxui/widget/_WidgetBase',
+    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/html', 'dojo/_base/event'
+    
+], function (declare, _WidgetBase,
+	dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, 
+	lang, text, html, event) {
+    'use strict';	
+		
+    // Declare widget"s prototype.
+    return declare("URLRedirectorOptions.widget.URLRedirectorOptions", [_WidgetBase], {
+
+        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
+        _handle: null,
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
+		
+		_attrHandle: null,
+		_windowRef: null,
+		_command: null,
+		
+        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
+        constructor: function () {
+            this._handles = [];
+        },
+
+        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
+        postCreate: function () {
+            console.log(this.id + ".postCreate");
+        },
+
+        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
+        update: function (obj, callback) {
+            console.log(this.id + ".update");
+			var self = this;
+            this._contextObj = obj;
+            if (this._contextObj !== null) {
+				
+				//NOTES:
+				//subscription on attr only is not triggered
+				//use this.subscribe from _WidgetBase, it will remove old handles
+				this._attrHandle = this.subscribe({
+					guid: this._contextObj.getGuid(),
+					/*attr: this.URLAttribute,*/
+					callback: lang.hitch(this, function (guid) {
+						mx.data.get({
+							guid    : guid,
+							callback : lang.hitch(this, function(obj) {
+								this._contextObj = obj;
+								this._updateRendering(null);
+							})
+						});
+					})
+				});							
+				
+				this._updateRendering(callback);
+            } else {
+                callback();
+            }
+        },
+/*
+        applyContext: function(obj, callback) {
+			console.log(this.id + ".applyContext");
+            if (obj !== null) {
+				mx.data.get({
+                    guids    : [obj.getTrackId()],
+                    callback : function(objs) {
+                        if (objs.length === 1) {
+                            this._contextObj = objs[0];
+
+							this._attrHandle = this.subscribe({
+								guid: this._contextObj.getGuid(),
+								attr: this.URLAttribute,
+								callback: lang.hitch(this, function (guid, attr, attrValue) {
+									//this._updateRendering();
+									alert('attribute: ' + attr + ' changed to: ' + attrValue);
+								});
+							});							
+							
+                            this._updateRendering(callback);							
+						}
+                    }
+                }, this);
+
+			} else {
+                callback && callback();
+            }
+        },
+*/
+		
+        // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
+        enable: function () {},
+
+        // mxui.widget._WidgetBase.enable is called when the widget should disable editing. Implement to disable editing if widget is input widget.
+        disable: function () {},
+
+        // mxui.widget._WidgetBase.resize is called when the page"s layout is recalculated. Implement to do sizing calculations. Prefer using CSS instead.
+        resize: function (box) {},
+
+        // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
+        uninitialize : function() {
+        },
+
+        // Rerender the interface.
+        _updateRendering: function (callback) {
+            var url = null;
+            try {
+                if (this.URLAttribute !== "" || this.urlprefix !== "")
+                {
+                    if(this.URLAttribute !=="") {
+                        url = this.urlprefix + this._contextObj.get(this.URLAttribute);
+                    } else {
+                        url = this.urlprefix;
+                    }
+                    this._redirectTo(url, callback);
+                } else {
+                    console.warning("At least prefix property or either URLAttribute property should be configured in widget property configuration");
+                    callback();
+                }
+            }
+            catch (err) {
+                console.error(this.id +"_updateRendering: " + err);
+                callback();
+            }
+        },
+
+        _redirectTo : function(url, callback) {
+            callback && callback();
+			if(url) {
+
+				if(this.CMDAttribute)
+					this._command = this._contextObj.get(this.CMDAttribute);
+				
+				if(this._command == 'redirect')
+				{
+					window.location.replace(url);
+				}
+				else if(this._command == 'close')
+				{
+					if(this._windowRef)
+						this._windowRef.close();
+					this._windowRef = null;
+				}
+				else
+				{
+					this._windowRef = window.open(url, this.target, this.options);
+				}
+			}
+        }
+    });
+});
+require(["URLRedirectorOptions/widget/URLRedirectorOptions"], function () {
+    "use strict";
+});
+
